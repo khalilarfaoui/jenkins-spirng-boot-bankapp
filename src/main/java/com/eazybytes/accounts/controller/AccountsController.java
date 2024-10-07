f7f0718b2a702e64cb4c7f137542a2db9a952981
khalilarfaoui <khalilwin1920@gmail.com>
2024-10-07 19:33:35 +0100

    simple-change4

--- src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -104,7 +104,7 @@ public class AccountsController {
     )
     @GetMapping("/test-change")
     public String fetchAccountDetails() {
-        return "Khalil arfaoui test 2 :))))))))!";
+        return "!!!!!!!!!!!!!!!!!!!";
     }
 
     @Operation(
